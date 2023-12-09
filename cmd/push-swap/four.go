032ec79dd312ce88f274ac8ffcfcafb252c669ae
pjunstall <pjtunstall@gmail.com>
2023-12-09 22:10:26 +0000

    better, hardcoded four and better test_four

--- cmd/push-swap/four.go
+++ cmd/push-swap/four.go
@@ -2,30 +2,106 @@ package main
 
 import (
 	"push-swap/ps"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func four(a, b ps.Stack) []string {
-	result := []string{"pb"}        // Push top two to B.
-	stayers := a.Nums[1:]           // The three that stay in A.
-	_, stayersRot := three(stayers) // `stayersRot` is true if no swap is needed to sort A.
+	nums := make([]int, len(a.Nums))
+	strs := make([]string, len(nums))
+	copy(nums, a.Nums)
+	sort.Ints(nums)
 
-	if !stayersRot {
-		stayers[0], stayers[1] = stayers[1], stayers[0]
-		result = append(result, "sa")
+	rank := make(map[int]int)
+	for i, v := range nums {
+		rank[v] = i + 1
 	}
 
-	// Consider maxB at top of B now, unless combineRotation is true.
-	switch fitTheFourth(a.Nums[0], stayers) {
-	case 1:
-		result = append(result, "ra")
-		stayers = append(stayers[1:], stayers[0])
-	case 2:
-		result = append(result, "rra")
-		stayers = append(stayers[len(stayers)-1:], stayers[:len(stayers)-1]...)
+	for i, v := range a.Nums {
+		strs[i] = strconv.Itoa(rank[v])
 	}
-	result = append(result, "pa")
-	a.Nums = append(a.Nums[:1], stayers...)
-	result = append(result, justRotate(a)...)
+	s := strings.Join(strs, " ")
 
-	return result
+	switch s {
+	case "1 2 3 4":
+		return []string{}
+	case "1 2 4 3":
+		return []string{"pb", "sa", "ra", "pa"}
+	case "1 3 2 4":
+		return []string{"pb", "sa", "pa"}
+	case "1 3 4 2":
+		return []string{"rra", "sa"}
+	case "1 4 2 3":
+		return []string{"sa", "ra"}
+	case "1 4 3 2":
+		return []string{"pb", "sa", "rra", "pa"}
+
+	case "2 1 3 4":
+		return []string{"sa"}
+	case "2 1 4 3":
+		return []string{"pb", "pb", "ss", "pa", "pa"}
+	case "2 3 1 4":
+		return []string{"ra", "ra", "sa", "ra"}
+	case "2 3 4 1":
+		return []string{"rra"}
+	case "2 4 1 3":
+		return []string{"sa", "ra", "sa"}
+	case "2 4 3 1":
+		return []string{"sa", "rra", "sa", "ra"}
+
+	case "3 1 2 4":
+		return []string{"sa", "ra", "sa", "rra"}
+	case "3 1 4 2":
+		return []string{"sa", "rra", "sa"}
+	case "3 2 1 4":
+		return []string{"sa", "ra", "ra", "sa", "ra"}
+	case "3 2 4 1":
+		return []string{"sa", "rra"}
+	case "3 4 1 2":
+		return []string{"ra", "ra"}
+	case "3 4 2 1":
+		return []string{"ra", "ra", "sa"}
+
+	case "4 1 2 3":
+		return []string{"ra"}
+	case "4 1 3 2":
+		return []string{"ra", "ra", "sa", "rra"}
+	case "4 2 1 3":
+		return []string{"ra", "sa"}
+	case "4 2 3 1":
+		return []string{"rra", "sa", "ra"}
+	case "4 3 1 2":
+		return []string{"sa", "ra", "ra"}
+	case "4 3 2 1":
+		return []string{"sa", "ra", "ra", "sa"}
+	}
+
+	return []string{}
 }
+
+// func four(a, b ps.Stack) []string {
+// 	result := []string{"pb"}        // Push top two to B.
+// 	stayers := a.Nums[1:]           // The three that stay in A.
+// 	_, stayersRot := three(stayers) // `stayersRot` is true if no swap is needed to sort A.
+
+// 	if !stayersRot {
+// 		stayers[0], stayers[1] = stayers[1], stayers[0]
+// 		result = append(result, "sa")
+// 	}
+
+// 	// Consider maxB at top of B now, unless combineRotation is true.
+// 	switch fitTheFourth(a.Nums[0], stayers) {
+// 	case 1:
+// 		result = append(result, "ra")
+// 		stayers = append(stayers[1:], stayers[0])
+// 	case 2:
+// 		result = append(result, "rra")
+// 		stayers = append(stayers[len(stayers)-1:], stayers[:len(stayers)-1]...)
+// 	}
+// 	result = append(result, "pa")
+// 	a.Nums = append(a.Nums[:1], stayers...)
+// 	result = append(result, justRotate(a)...)
+
+// 	return result
+// }
--- cmd/push-swap/four_test.go
+++ cmd/push-swap/four_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 	"testing"
@@ -11,12 +12,12 @@ import (
 
 func TestFour(t *testing.T) {
 	tests := []string{
-		"1 2 3 4",
+		"-999 0 999 1000000001",
 		"1 2 4 3",
 		"1 3 2 4",
 		"1 3 4 2",
 		"1 4 2 3",
-		"1 4 3 2",
+		"-115 400 360 0",
 
 		"2 1 3 4",
 		"2 1 4 3",
@@ -41,6 +42,13 @@ func TestFour(t *testing.T) {
 	}
 
 	for _, tc := range tests {
+		expectedStr := strings.Split(tc, " ")
+		expectedInt := make([]int, len(expectedStr))
+		for i, v := range expectedStr {
+			expectedInt[i], _ = strconv.Atoi(v)
+		}
+		sort.Ints(expectedInt)
+		expected := fmt.Sprintf("%v", expectedInt)
 		aInit, _ := ps.NewStack(tc)
 		bInit, _ := ps.NewStack("")
 		instructions := four(aInit, bInit)
@@ -50,19 +58,22 @@ func TestFour(t *testing.T) {
 		if err != nil {
 			t.Errorf("on initial stack %v, `four` returned error: %v", tc, err)
 		}
-		fmt.Println(tc, a.Nums, b.Nums, instructions, a.Top)
 		arrInt := append(a.Nums[a.Top:], a.Nums[:a.Top]...)
 		arrStr := make([]string, len(a.Nums))
 		problem := false
 		for i, v := range arrInt {
 			arrStr[i] = strconv.Itoa(v)
-			if v != i+1 {
+			if i == len(arrInt)-1 {
+				break
+			}
+			if v > arrInt[i+1] {
 				problem = true
 			}
 		}
 		str := strings.Join(arrStr, " ")
 		if problem {
-			t.Errorf("expected %v, got %v", "1 2 3 4", str)
+			// fmt.Println(tc, instructions, arrInt)
+			t.Errorf("expected %v, got %v", expected, str)
 		}
 	}
 }
