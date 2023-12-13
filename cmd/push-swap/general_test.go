acbcc5e022f169c075e1643b26cc8ed35fcf300e
pjunstall <pjtunstall@gmail.com>
2023-12-13 12:05:46 +0000

    testing 5 random numbers 100 times, 100 random numbers 100 times, and all permutations of the numbers 1-6

--- cmd/push-swap/general_test.go
+++ cmd/push-swap/general_test.go
@@ -10,7 +10,28 @@ import (
 	"testing"
 )
 
+// 100 times tests that 5 random numbers are sorted in 12 or fewer instructions,
+// 100 times tests that 100 random numbers are sorted in 700 or fewer instructions,
+// and 6! times tests that all permutations of 1-6 are sorted in 12 or fewer instructions:
 func TestGeneral(t *testing.T) {
+	// Check length of result for 5 random numbers 100 times:
+	for i := 0; i < 100; i++ {
+		five := fiveRandomNumbers()
+		a, err := ps.NewStack(five)
+		if err != nil {
+			t.Errorf("general(%s) failed: %s", five, err)
+		}
+		b, _ := ps.NewStack("")
+		instructions := general(&a, &b)
+		_, sorted := ps.Check(a, b)
+		if !sorted {
+			t.Errorf("not sorted")
+		}
+		if len(instructions) >= 12 {
+			t.Errorf("%v instructions--that's a bit much", len(instructions))
+		}
+	}
+
 	// Check length of result for 100 random numbers 100 times:
 	for i := 0; i < 100; i++ {
 		hundred := hundredRandomNumbers()
@@ -47,8 +68,8 @@ func TestGeneral(t *testing.T) {
 							b, _ := ps.NewStack("")
 							general(&a, &b)
 							// instructions := general(&a, &b)
-							// if len(instructions) > 12 {
-							// 	t.Errorf("%v took more than 12 instructions to sort", input)
+							// if len(instructions) => 12 {
+							// 	t.Errorf("%v took 12 more instructions to sort", input)
 							// }
 							_, sorted := ps.Check(a, b)
 							if !sorted {
@@ -71,10 +92,37 @@ func TestGeneral(t *testing.T) {
 	}
 }
 
+// The first 100 primes greater than 100:
+var primes = []int{101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997}
+
+func fiveRandomNumbers() string {
+	var result string
+	var arr [5]int
+
+	rand.Shuffle(len(primes), func(i, j int) { primes[i], primes[j] = primes[j], primes[i] })
+
+	// Choose 5 random numbers between -100 and 100:
+	for i := 0; i < 5; i++ {
+		r := randInt(-99, 100) * primes[i]
+		if r == 0 {
+			i--
+			continue
+		}
+		arr[i] = r
+	}
+
+	for i, v := range arr {
+		result += fmt.Sprintf("%d", v)
+		if i != 4 {
+			result += " "
+		}
+	}
+
+	return result
+}
+
 // Generate a string of a hundred random numbers:
 func hundredRandomNumbers() string {
-	// The first 100 primes greater than 100:
-	primes := []int{101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997}
 	var result string
 	var arr [100]int
 
