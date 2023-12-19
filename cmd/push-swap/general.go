1644b49a693a8db21756f472ca0bd943cd9686f8
pjunstall <pjtunstall@gmail.com>
2023-12-19 11:46:03 +0000

    combined sortToA and sortToB into a common function, merge

--- cmd/push-swap/general.go
+++ cmd/push-swap/general.go
@@ -43,13 +43,15 @@ func general(a, b *ps.Stack) []string {
 		ps.Sx(b)
 		result = append(result, "sb")
 	}
-	result = append(result, sortToB(a, b)...)
+	// result = append(result, sortToB(a, b)...)
+	result = append(result, merge(a, b, 3, true)...)
 	_, rotatable := three(a.Nums)
 	if !rotatable {
 		ps.Sx(a)
 		result = append(result, "sa")
 	}
-	result = append(result, sortToA(a, b)...)
+	// result = append(result, sortToA(a, b)...)
+	result = append(result, merge(b, a, 0, false)...)
 
 	result = append(result, justRotate(*a)...)
 	ps.Run(a, b, justRotate(*a))
@@ -58,7 +60,7 @@ func general(a, b *ps.Stack) []string {
 	// swaps and rotations. This uses a BFS, and if too slow for big
 	// stacks, hence the simpler checks at the beginning of this
 	// function.
-	if len(a.Nums) < 32 {
+	if len(a.Nums) < 9 {
 		alt := bfs(c, len(result))
 		if len(alt) > 0 && len(alt) < len(result) {
 			result = alt
@@ -68,14 +70,21 @@ func general(a, b *ps.Stack) []string {
 	return result
 }
 
-func sortToB(a, b *ps.Stack) []string {
+// Sorts as it merges from one stack to another. Here a and
+// b are just parameters, with a representing the source stack
+// and b the destination stack. The stopAt parameter is the number
+// of elements to leave in the source stack. If forward is true,
+// the merge is from the real a to the real b, otherwise it is
+// from the real b to the real a.
+func merge(a, b *ps.Stack, stopAt int, forward bool) []string {
 	result := []string{}
+
 	for {
 		A := a.GetNumsSlice()
 		B := b.GetNumsSlice()
 		journeyPlanner := make([]ps.PushInfo, len(A))
 
-		if len(A) == 3 {
+		if len(A) == stopAt {
 			break
 		}
 
@@ -94,19 +103,38 @@ func sortToB(a, b *ps.Stack) []string {
 				ra = true
 			}
 
-			foundOneLess := false
-			targetIndex, targetValue := ps.MinInt(B)
-			for j, w := range B {
-				if w < v {
-					foundOneLess = true
-					if w > targetValue {
-						targetValue = w
-						targetIndex = j
+			var targetIndex, targetValue int
+
+			if forward {
+				foundOneLess := false
+				targetIndex, targetValue = ps.MinInt(B)
+				for j, w := range B {
+					if w < v {
+						foundOneLess = true
+						if w > targetValue {
+							targetValue = w
+							targetIndex = j
+						}
 					}
 				}
-			}
-			if !foundOneLess {
+				if !foundOneLess {
+					targetIndex, targetValue = ps.MaxInt(B)
+				}
+			} else {
+				foundOneGreater := false
 				targetIndex, targetValue = ps.MaxInt(B)
+				for j, w := range B {
+					if w > v {
+						foundOneGreater = true
+						if w < targetValue {
+							targetValue = w
+							targetIndex = j
+						}
+					}
+				}
+				if !foundOneGreater {
+					targetIndex, targetValue = ps.MinInt(B)
+				}
 			}
 
 			if targetIndex > len(B)/2 {
@@ -116,133 +144,6 @@ func sortToB(a, b *ps.Stack) []string {
 				cost += targetIndex
 				stepsB = targetIndex
 				rb = true
-			}
-
-			jointSteps = min(stepsA, stepsB)
-
-			// Optimization to take advantage of combined rotatioms
-			// when it makes no difference which direction we rotate a stack.
-			if len(A)%2 == 0 && stepsA == len(A)/2 {
-				ra = rb
-			}
-			if len(B)%2 == 0 && stepsB == len(B)/2 {
-				rb = ra
-			}
-
-			if (ra && rb) || (!ra && !rb) {
-				cost -= jointSteps
-				stepsA -= jointSteps
-				stepsB -= jointSteps
-			}
-
-			journeyPlanner[i] = ps.PushInfo{
-				Value:       v,
-				TargetIndex: targetIndex,
-				TargetValue: targetValue,
-				Cost:        cost,
-				Ra:          ra,
-				Rb:          rb,
-				StepsA:      stepsA,
-				StepsB:      stepsB,
-				JointSteps:  jointSteps}
-
-			if cost == 0 {
-				break
-			}
-			if cost < journeyPlanner[cheapest].Cost {
-				cheapest = i
-			}
-		}
-
-		c := journeyPlanner[cheapest]
-
-		if c.Ra && c.Rb {
-			for j := 0; j < c.JointSteps; j++ {
-				ps.Rr(b, a)
-				result = append(result, "rr")
-			}
-		} else if !c.Ra && !c.Rb {
-			for j := 0; j < c.JointSteps; j++ {
-				ps.Rrr(a, b)
-				result = append(result, "rrr")
-			}
-		}
-		if c.Ra {
-			for j := 0; j < c.StepsA; j++ {
-				ps.Rx(a)
-				result = append(result, "ra")
-			}
-		} else {
-			for j := 0; j < c.StepsA; j++ {
-				ps.Rrx(a)
-				result = append(result, "rra")
-			}
-		}
-		if c.Rb {
-			for j := 0; j < c.StepsB; j++ {
-				ps.Rx(b)
-				result = append(result, "rb")
-			}
-		} else {
-			for j := 0; j < c.StepsB; j++ {
-				ps.Rrx(b)
-				result = append(result, "rrb")
-			}
-		}
-		ps.Px(b, a)
-		result = append(result, "pb")
-	}
-
-	return result
-}
-
-func sortToA(a, b *ps.Stack) []string {
-	result := []string{}
-	for {
-		A := a.GetNumsSlice()
-		B := b.GetNumsSlice()
-		journeyPlanner := make([]ps.PushInfo, len(B))
-
-		if len(B) == 0 {
-			break
-		}
-
-		cheapest := 0
-		for i, v := range B {
-			var cost int
-			var ra, rb bool
-			var stepsA, stepsB, jointSteps int
-
-			if i > len(B)/2 {
-				cost = len(B) - i
-			} else {
-				cost = i
-				rb = true
-			}
-			stepsB = cost
-
-			foundOneGreater := false
-			targetIndex, targetValue := ps.MaxInt(A)
-			for j, w := range A {
-				if w > v {
-					foundOneGreater = true
-					if w < targetValue {
-						targetValue = w
-						targetIndex = j
-					}
-				}
-			}
-			if !foundOneGreater {
-				targetIndex, targetValue = ps.MinInt(A)
-			}
-
-			if targetIndex > len(A)/2 {
-				cost += len(A) - targetIndex
-				stepsA = len(A) - targetIndex
-			} else {
-				cost += targetIndex
-				stepsA = targetIndex
-				ra = true
 			}
 
 			jointSteps = min(stepsA, stepsB)
@@ -297,27 +198,52 @@ func sortToA(a, b *ps.Stack) []string {
 		if c.Ra {
 			for j := 0; j < c.StepsA; j++ {
 				ps.Rx(a)
-				result = append(result, "ra")
+				if forward {
+					result = append(result, "ra")
+				} else {
+					result = append(result, "rb")
+				}
 			}
 		} else {
 			for j := 0; j < c.StepsA; j++ {
 				ps.Rrx(a)
-				result = append(result, "rra")
+				if forward {
+					result = append(result, "rra")
+				} else {
+					result = append(result, "rrb")
+				}
 			}
 		}
 		if c.Rb {
 			for j := 0; j < c.StepsB; j++ {
 				ps.Rx(b)
-				result = append(result, "rb")
+				if forward {
+					result = append(result, "rb")
+				} else {
+					result = append(result, "ra")
+				}
 			}
 		} else {
 			for j := 0; j < c.StepsB; j++ {
 				ps.Rrx(b)
-				result = append(result, "rrb")
+				if forward {
+					result = append(result, "rrb")
+				} else {
+					result = append(result, "rra")
+				}
 			}
 		}
-		ps.Px(a, b)
-		result = append(result, "pa")
+		if forward {
+			ps.Px(b, a)
+			result = append(result, "pb")
+		} else {
+			if len(result) > 0 && result[len(result)-1] == "pb" {
+				result = result[:len(result)-1]
+			} else {
+				result = append(result, "pa")
+				ps.Px(b, a)
+			}
+		}
 	}
 
 	return result
